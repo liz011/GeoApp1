72bad03bcd82f1760fc1ffedf2069217e7ad019c
liz011 <111695288+liz011@users.noreply.github.com>
2022-12-27 04:28:51 -0600

    Update lista.kt

--- app/src/main/java/com/lrr/geoapp/lista.kt
+++ app/src/main/java/com/lrr/geoapp/lista.kt
@@ -1,11 +1,20 @@
 package com.lrr.geoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class lista : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista)
+
+        var btnI: Button = findViewById(R.id.btnI)
+
+        val intent = Intent(this,Recorrido1::class.java)
+        startActivity(intent)
     }
 }
